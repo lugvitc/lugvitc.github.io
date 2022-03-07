c05865efaf64ae77e5caf88aab19631e04e3df15
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-03-07 13:26:53 +0530

    added dock icon for games (remove if not needed); topBar improved for small screens; topBar clock now updates every second

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -2,38 +2,35 @@ import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './topBar.css';
+import TopBarTimeDate from './topBarDate';
 
 export default function TopBar({navs}) {
     const [activeIndex, setActiveIndex] = useState(0);
 
-    // if these variables are set to be regularly updated, implement them using hooks
-    // until then, constants are fine
-    const date = "Sun 6 March";
-    const time = "2:08 AM"
-    // const [time, setTime] = useState("4:20pm");
-    // const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
+    const topBarElementClass = (activeIndex, index) =>
+        "top-bar-element " + (
+            activeIndex === index
+                ? "top-bar-element-active"
+                : "top-bar-element-inactive"
+        );
 
     return (
         <div className = "top-bar">
             <div className = "top-bar-element-container">
-                {navs.map((nav, num) =>
+                {navs.map((nav, index) =>
                     <Link
-                        key = {num}
-                        className = {
-                            "top-bar-element " + 
-                            (activeIndex === num
-                                ? "top-bar-element-active"
-                                : "top-bar-element-inactive"
-                            )
-                        }
+                        key = {index}
+                        className = {topBarElementClass(activeIndex, index)}
                         to = {nav.link}
-                        onClick = {() => setActiveIndex(num)}
+                        onClick = {() => setActiveIndex(index)}
                     >
                         {nav.title} ▼
                     </Link>
                 )}
             </div>
-            <div className = "top-bar-element-non-clickable">{`${time}, ${date}`}</div>
+            <div className = "top-bar-element-non-clickable">
+                <TopBarTimeDate/>
+            </div>
         </div>
     );
 }
--- src/content/dockItems.js
+++ src/content/dockItems.js
@@ -2,6 +2,7 @@ import InstagramIcon from '../images/Instagram.svg';
 import LinkedInIcon from '../images/LinkedIn.svg';
 import GithubIcon from '../images/Github.svg';
 import DiscordIcon from '../images/Discord.svg';
+import GamesIcon from '../images/Games.svg';
 
 export const dockItems = [
     {
@@ -19,5 +20,9 @@ export const dockItems = [
     {
         link: "https://discord.com/channels/949736566899744778/949736566899744781",
         icon: DiscordIcon
+    },
+    {
+        link: "https://en.wikipedia.org/wiki/Game",
+        icon: GamesIcon
     }
 ];
