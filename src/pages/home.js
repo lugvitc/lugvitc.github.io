120bea155f6e9d54b74598a25fb7bb8d396c7f19
3V3RYONE <beleswarprasad@gmail.com>
2022-05-22 16:27:44 +0530

    add whole transition theme to home site text

--- src/pages/home.js
+++ src/pages/home.js
@@ -14,8 +14,12 @@ export default function Home() {
 
     return (
         <TerminalWindow>
-            <TerminalPrompt path = "~"> sudo lug </TerminalPrompt>
-            [sudo] password for lugvitc: <span className = "terminal-text" style = {{color: Colors.nord11}}> **********</span><br/>
+            <TerminalPrompt path = "~"> <span className="typeWriteFirstText">{typingText}</span> </TerminalPrompt>
+	    { typingText=='sudo lug' ? (
+            <div> [sudo] password for lugvitc: <span style = {{color: Colors.nord11}} className="typeWriteSecondText"> {typingSecondText} </span><br/>
+	    { typingSecondText=='***************' ? (
+	    <div>
+	    <span style = {{color: Colors.nord14}}>access granted...</span><br/>
 
             <h1 className = "terminal-text">Linux Club VIT, Chennai</h1>
             The 'Linux Club' of VIT, Chennai was created to Spread the awareness of Free and Open Source Software (FOSS) through Linux flavours.
@@ -24,6 +28,19 @@ export default function Home() {
             It aims to get members to contribute to FOSS, make them more industry ready and to promote GNU/Linux in General.<br/>
 
             <br/>
+	    <span style = {{color: Colors.nord11}}>L</span>
+            <span style = {{color: Colors.nord12}}>I</span>
+            <span style = {{color: Colors.nord13}}>N</span>
+            <span style = {{color: Colors.nord14}}>U</span>
+            <span style = {{color: Colors.nord10}}>X</span>
+            <span style = {{color: Colors.nord15}}>!</span>
+	    </div>
+	    ) : (
+		    <div> </div>
+	    ) }
+	    ) : (
+		    <div> </div>
+	    ) }
         </TerminalWindow>
     );
 }
