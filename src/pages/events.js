ec2eef9f429a4a4b169a9013e2d22a00c08a6468
3V3RYONE <beleswarprasad@gmail.com>
2022-05-23 21:12:51 +0530

    add minimal typing animation

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,14 +3,26 @@ import LugLink from '../components/terminal/lugLink';
 import TerminalPrompt from '../components/terminal/terminalPrompt';
 import TerminalWindow from '../components/terminal/terminalWindow';
 import { eventsDetails } from '../content/eventsDetails';
+import '../components/typeWriter.css';
+import {TypeAnimation} from '../components/typeWriter.js';
+
+const firstText = 'cd events'
+const secondText = 'cat events.txt'
 
 export default function Events() {
+	const typingText = TypeAnimation(firstText)
+	const typingSecondText = TypeAnimation(secondText)
     return (
         <TerminalWindow>
             <section id = "terminal">
-                <TerminalPrompt path = "~">cd events</TerminalPrompt>
-                <TerminalPrompt path = "~/events">cat events.txt</TerminalPrompt>
-            </section>
+                <TerminalPrompt path = "~"> <span className="typeWriteFirstText">{typingText}</span> </TerminalPrompt>
+	    //</section>
+	    { typingText=='cd events' ? (
+		    <div>
+                <TerminalPrompt path = "~/events"> <span className="typeWriteSecondText"> {typingSecondText} </span></TerminalPrompt>
+		</section>
+	    { typingSecondText=='cat events.txt' ? (
+		    <div>
             {eventsDetails.map(({number, name, link, details, id}, index) =>
                 <section    
                     className = "terminal-text"
@@ -23,6 +35,15 @@ export default function Events() {
                     <span>{details}</span>
                 </section>
             )}
+		    </div>
+	    ) : (
+		    <div> </div>
+	    ) }
+		    </div>
+	    ) : (
+		    <div> </section> </div>
+	    ) }
+
         </TerminalWindow>
     );
 }
