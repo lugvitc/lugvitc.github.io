b4d90db518b8b2c8022b7843b96fb891d71a4cd5
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-06-29 00:31:20 +0530

    ShowAfterTypwriter takes local storage value to check whether to show animations

--- src/components/showAfterTypewriter/showAfterTypwriter.js
+++ src/components/showAfterTypewriter/showAfterTypwriter.js
@@ -4,18 +4,22 @@ import './showAfterTypewriter.css';
 
 export default function ShowAfterTypewriter({ text, textContainer, children }) {
     const [typingText, setTypingText] = useState('');
+    const showAnimations =
+        window.localStorage.getItem('show-animations') === 'true';
 
     useEffect(() => {
-        const nextTypingText = text.slice(0, typingText.length + 1);
-        if (nextTypingText === typingText) return;
+        if (showAnimations) {
+            const nextTypingText = text.slice(0, typingText.length + 1);
+            if (nextTypingText === typingText) return;
 
-        const timeOut = setTimeout(() => {
-            setTypingText(text.slice(0, typingText.length + 1));
-        }, 250);
-        return () => clearTimeout(timeOut);
+            const timeOut = setTimeout(() => {
+                setTypingText(text.slice(0, typingText.length + 1));
+            }, 250);
+            return () => clearTimeout(timeOut);
+        }
     }, [text, typingText]);
 
-    if (typingText !== text)
+    if (showAnimations && typingText !== text)
         return textContainer(
             <span className='typewriter-text'>{typingText}</span>
         );
