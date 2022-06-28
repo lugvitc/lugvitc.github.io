8d910c6b7ed416e493ea587679154d3b0712cd47
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-06-29 00:32:24 +0530

    terminal window puts the time at which the animation ends in localstorage

--- src/components/terminal/terminalWindow.js
+++ src/components/terminal/terminalWindow.js
@@ -1,3 +1,6 @@
+import { useCallback, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import TerminalPrompt from './terminalPrompt.js';
 import ShowAfterTypewriter from '../showAfterTypewriter/showAfterTypwriter.js';
 
@@ -6,6 +9,18 @@ import './terminal.css';
 import './terminalText.css';
 
 export default function TerminalWindow({ prompts, children }) {
+    const location = useLocation();
+
+    const ref = useCallback(node => {
+        if (node) {
+            console.log(location.pathname);
+            window.localStorage.setItem(
+                location.pathname,
+                new Date().toISOString()
+            );
+        }
+    }, []);
+
     return (
         <div className='terminal'>
             <div className='terminal-dots'>
@@ -36,7 +51,7 @@ export default function TerminalWindow({ prompts, children }) {
                             {acc}
                         </ShowAfterTypewriter>
                     ),
-                    children
+                    <div ref={ref}>{children}</div>
                 )}
             </div>
         </div>
