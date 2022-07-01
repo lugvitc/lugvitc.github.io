7f45bca9cc7174a6228abff84a562308a1060f8e
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-07-01 12:01:33 +0530

    implemented useSettings using zustand

--- src/hooks/useSettings.js
+++ src/hooks/useSettings.js
@@ -1,8 +1,42 @@
-export default function useSettings() {
-    const animationsOn =
-        window.localStorage.getItem('animations-on') === 'true';
-    const terminalDotsOnLeft =
-        window.localStorage.getItem('terminal-dots-on-left') === 'true';
+import create from 'zustand';
 
-    return { animationsOn, terminalDotsOnLeft };
+export default function useSettings() {
+    const terminalDotsOnLeftStore = create(set => ({
+        terminalDotsOnLeft:
+            window.localStorage.getItem('terminal-dots-on-left') === 'true',
+        setTerminalDotsOnLeft: val => {
+            window.localStorage.setItem(
+                'terminal-dots-on-left',
+                val ? 'true' : 'false'
+            );
+            set(state => ({ terminalDotsOnLeft: val }));
+        }
+    }));
+
+    const animationsOnStore = create(set => ({
+        animationsOn: window.localStorage.getItem('animations-on') === 'true',
+        setAnimationsOn: val => {
+            window.localStorage.setItem(
+                'animtaions-on',
+                val ? 'true' : 'false'
+            );
+            set(state => ({ animationsOn: val }));
+        }
+    }));
+
+    const animationsOn = animationsOnStore(state => state.animationsOn);
+    const terminalDotsOnLeft = terminalDotsOnLeftStore(
+        state => state.terminalDotsOnLeft
+    );
+
+    return {
+        animationsOn: animationsOnStore(state => state.animationsOn),
+        setAnimationsOn: animationsOnStore(state => state.setAnimationsOn),
+        terminalDotsOnLeft: terminalDotsOnLeftStore(
+            state => state.terminalDotsOnLeft
+        ),
+        setTerminalDotsOnLeft: terminalDotsOnLeftStore(
+            state => state.setTerminalDotsOnLeft
+        )
+    };
 }
