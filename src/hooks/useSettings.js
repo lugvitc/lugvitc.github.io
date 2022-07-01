650742f967ccd0a65e1f540465e19a873e80d72e
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-07-01 16:52:12 +0530

    zustand stores put outside of hook function

--- src/hooks/useSettings.js
+++ src/hooks/useSettings.js
@@ -1,38 +1,35 @@
 import create from 'zustand';
 
+const terminalDotsOnLeftStore = create(set => ({
+    terminalDotsOnLeft:
+        window.localStorage.getItem('terminal-dots-on-left') === 'true',
+    setTerminalDotsOnLeft: val => {
+        window.localStorage.setItem(
+            'terminal-dots-on-left',
+            val ? 'true' : 'false'
+        );
+        set(state => ({ terminalDotsOnLeft: val }));
+    }
+}));
+
+const animationsOnStore = create(set => ({
+    animationsOn: window.localStorage.getItem('animations-on') === 'true',
+    setAnimationsOn: val => {
+        window.localStorage.setItem('animations-on', val ? 'true' : 'false');
+        set(state => ({ animationsOn: val }));
+    }
+}));
+
+const setDefaults = () => {
+    if (!window.localStorage.getItem('animations-on')) {
+        window.localStorage.setItem('animations-on', 'true');
+    }
+    if (!window.localStorage.getItem('terminal-dots-on-left')) {
+        window.localStorage.setItem('terminal-dots-on-left', 'true');
+    }
+};
+
 export default function useSettings() {
-    const terminalDotsOnLeftStore = create(set => ({
-        terminalDotsOnLeft:
-            window.localStorage.getItem('terminal-dots-on-left') === 'true',
-        setTerminalDotsOnLeft: val => {
-            window.localStorage.setItem(
-                'terminal-dots-on-left',
-                val ? 'true' : 'false'
-            );
-            set(state => ({ terminalDotsOnLeft: val }));
-        }
-    }));
-
-    const animationsOnStore = create(set => ({
-        animationsOn: window.localStorage.getItem('animations-on') === 'true',
-        setAnimationsOn: val => {
-            window.localStorage.setItem(
-                'animations-on',
-                val ? 'true' : 'false'
-            );
-            set(state => ({ animationsOn: val }));
-        }
-    }));
-
-    const setDefaults = () => {
-        if (!window.localStorage.getItem('animations-on')) {
-            window.localStorage.setItem('animations-on', 'true');
-        }
-        if (!window.localStorage.getItem('terminal-dots-on-left')) {
-            window.localStorage.setItem('terminal-dots-on-left', 'true');
-        }
-    };
-
     return {
         setDefaults,
         animationsOn: animationsOnStore(state => state.animationsOn),
