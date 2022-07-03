bd7ad0f6f2d520191f48d0326a39d1c4e4860de9
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-07-03 18:07:57 +0530

    routes are now in index.js; App.js has the layout

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import { useEffect, useRef, useState } from 'react';
-import { HashRouter, Routes, Route, Outlet } from 'react-router-dom';
-
-import Home from './pages/home';
-import Events from './pages/events/events';
 
 import TopBar from './components/topBar/topBar';
 import Dock from './components/dock/dock';
-import LearnLinux from './pages/learnLinux/learnLinux';
 
 import SettingsDialog from './components/settingsDialog/settingsDialog';
 import useKeyboardShortcuts from './hooks/useKeyboardShortcuts';
 // import UserForm from './pages/recruitment_2022/recruitment';
 
-export default function App() {
-    const pages = [
-        {
-            title: 'Home',
-            link: '/',
-            component: <Home />
-        },
-        {
-            title: 'Events',
-            link: '/events',
-            component: <Events />
-        },
-        {
-            title: 'Learn Linux',
-            link: '/learn-linux',
-            component: <LearnLinux />
-        }
-        //, {
-        //     title: 'Recruitment 2022',
-        //     link: '/recruitment',
-        //     component: <UserForm />
-        // }
-    ];
-
+export default function App({ children, pages }) {
     useKeyboardShortcuts();
 
     const [mainTopMargin, setMainTopMargin] = useState('0');
@@ -53,43 +25,20 @@ export default function App() {
     }, []);
 
     return (
-        <HashRouter basename={process.env.PUBLIC_URL}>
-            <Routes>
-                <Route
-                    path='/'
-                    element={
-                        <>
-                            <TopBar refer={topBarRef} topBarLinks={pages} />
-                            <SettingsDialog />
-                            <main
-                                style={{
-                                    marginTop: mainTopMargin,
-                                    minHeight: `calc(100vh - ${mainTopMargin})`
-                                }}
-                                id='content'
-                            >
-                                <Dock />
-                                <div id='terminals'>
-                                    <Outlet />
-                                </div>
-                            </main>
-                        </>
-                    }
-                >
-                    {pages.map((page, index) =>
-                        page.link === '/' ? (
-                            <Route key={index} index element={page.component} />
-                        ) : (
-                            <Route
-                                key={index}
-                                path={page.link}
-                                element={page.component}
-                            />
-                        )
-                    )}
-                </Route>
-            </Routes>
-        </HashRouter>
+        <>
+            <TopBar refer={topBarRef} topBarLinks={pages} />
+            <SettingsDialog />
+            <main
+                style={{
+                    marginTop: mainTopMargin,
+                    minHeight: `calc(100vh - ${mainTopMargin})`
+                }}
+                id='content'
+            >
+                <Dock />
+                <div id='terminals'>{children}</div>
+            </main>
+        </>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { HashRouter, Routes, Route, Outlet } from 'react-router-dom';
 
 import './styles/colors.css';
 import './styles/index.css';
 import './styles/forms.css';
 
+import App from './App';
+import Home from './pages/home';
+import Events from './pages/events/events';
+import LearnLinux from './pages/learnLinux/learnLinux';
+// import UserForm from './pages/recruitment_2022/recruitment';
+
+const pages = [
+    {
+        title: 'Home',
+        link: '/',
+        component: <Home />
+    },
+    {
+        title: 'Events',
+        link: '/events',
+        component: <Events />
+    },
+    {
+        title: 'Learn Linux',
+        link: '/learn-linux',
+        component: <LearnLinux />
+    }
+    //, {
+    //     title: 'Recruitment 2022',
+    //     link: '/recruitment',
+    //     component: <UserForm />
+    // }
+];
+
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <HashRouter basename={process.env.PUBLIC_URL}>
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <App pages={pages}>
+                            <Outlet />
+                        </App>
+                    }
+                >
+                    {pages.map((page, index) =>
+                        page.link === '/' ? (
+                            <Route key={index} index element={page.component} />
+                        ) : (
+                            <Route
+                                key={index}
+                                path={page.link}
+                                element={page.component}
+                            />
+                        )
+                    )}
+                </Route>
+            </Routes>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
