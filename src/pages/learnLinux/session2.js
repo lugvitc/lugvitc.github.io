b288c1e31c56c67c0307e5b7dcc3e1e223ec70c3
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-07-06 14:13:32 +0530

    session 2 content added

--- src/pages/learnLinux/session2.js
+++ src/pages/learnLinux/session2.js
@@ -4,9 +4,31 @@ import CodeBlock from '../../components/codeBlock/codeBlock';
 export default function Session2() {
     return (
         <>
-            type cd to switch directories:
-            <CodeBlock>cd</CodeBlock>
-            <CodeBlock>ls</CodeBlock>
+            <section>
+                <h2>What is the terminal?</h2>
+                <h3>History</h3>
+                <h3>Shells</h3>
+                <h3>Terminal Emulators</h3>
+            </section>
+            <section>
+                <h2>Terminal Basics</h2>
+                <h3>The prompt</h3>
+                <h3>Terminal Commands</h3>
+            </section>
+            <section>
+                <h2>Terminal Basics</h2>
+                <h3>Moving Around</h3>
+                <h3>History</h3>
+                <h3>Tab Completion</h3>
+                <h3>Manual Pages</h3>
+            </section>
+            <section>
+                <h2>Working with files</h2>
+                <h3>File types</h3>
+                <h3>File contents</h3>
+                <h3>File Management</h3>
+            </section>
         </>
     );
 }
+
