c114b25e65ade16745df74f6740aa640efd2843d
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-07-10 20:56:58 +0530

    added page structure for session 3

--- src/pages/learnLinux/session3.js
+++ src/pages/learnLinux/session3.js
@@ -1,3 +1,40 @@
 export default function Session3() {
-    return <></>;
+    return (
+        <>
+            <section>
+                <h2>The Filesystem</h2>
+
+                <h3>Filesystems</h3>
+                <p></p>
+
+                <h3>The Linux Filesystem Heirarchy (FSH)</h3>
+                <p></p>
+
+                <h3>File types</h3>
+
+                <h4>Plain Text</h4>
+                <p></p>
+                <h4>Binary</h4>
+                <p></p>
+                <h4>Directories</h4>
+                <p></p>
+                <h4>Synlinks</h4>
+                <p></p>
+            </section>
+            <section>
+                <h2>Users, Groups and Permissions</h2>
+
+                <h3>Users and Groups</h3>
+                <p></p>
+
+                <h3>Permissions</h3>
+                <p></p>
+
+                <h3></h3>
+            </section>
+            <section>
+                <h2>Processes</h2>
+            </section>
+        </>
+    );
 }
