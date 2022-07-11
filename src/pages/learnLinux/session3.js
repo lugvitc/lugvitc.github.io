6607f88bdec7faa851db24c20945ada3a2ee133e
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-07-11 21:23:57 +0530

    added some session 3 content

--- src/pages/learnLinux/session3.js
+++ src/pages/learnLinux/session3.js
@@ -2,13 +2,40 @@ export default function Session3() {
     return (
         <>
             <section>
-                <h2>The Filesystem</h2>
+                <h2>Files</h2>
 
                 <h3>Filesystems</h3>
-                <p></p>
+                <p>
+                    Any operating system needs to access and manipulate the
+                    files stored on the disk. However, for a computer, the data
+                    for on a disk is just a big blob of 1's and 0's. The OS
+                    needs to know which data is in which file and how to make
+                    sense of it. To solve this, data on a file is stored
+                    according to a set of rules called a <em>filesystem</em>.
+                </p>
+
+                <p>
+                    There are a lot of filesystems out there, some major ones
+                    are:
+                </p>
+                <ul>
+                    <li>FAT (File Allocation Table): ...</li>
+                    <li>
+                        NTFS (): A propietary filesystem used mainly by MS
+                        Windows.
+                    </li>
+                    <li>
+                        EXT family(ext2, ext3, ext4): Filesystems used by most
+                        Linux Distributions.
+                    </li>
+                </ul>
 
                 <h3>The Linux Filesystem Heirarchy (FSH)</h3>
-                <p></p>
+                <p>
+                    Those of you familiar with Windows would be used to files
+                    being stored in different <em>drives</em> shown by a letter.
+                    Theere isn't a single
+                </p>
 
                 <h3>File types</h3>
 
