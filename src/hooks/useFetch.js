85b1aa5abaa4c4b5520fc9de6a95d2bc84157130
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-08-02 13:34:22 +0530

    added apiPost method to useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,4 +1,18 @@
 export default function useFetch() {
-    const api = (input, init) => fetch('http://localhost:5000/api' + input, init);
-    return {api}
+    const apiURL = 'http://localhost:5000/api';
+    
+    const api = (path, init) => fetch(apiURL + path, init);
+
+    const apiPost = (path, object) => api(path, {
+        method: 'POST',
+        headers: {
+            ContentType: 'application/json'
+        },
+        body: JSON.stringify(object)
+    })
+
+    return {
+        api,
+        apiPost
+    }
 }
