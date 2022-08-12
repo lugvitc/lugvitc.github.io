ed138eab6421cf5d8f6b778d189fbf99034a3086
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-08-12 22:34:39 +0530

    added play page (and temp challenges page)

---
+++ src/pages/rescueTux2022/play/play.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+
+import TerminalWindow from '../../../components/terminal/terminalWindow';
+import Card from '../components/Card/Card';
+
+import styles from './play.module.css';
+
+export default function Play() {
+    const [challenges, setChallenges] = useState(null);
+    const tempChallenges = [
+        {
+            id: 1,
+            name: 'A hidden secret',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            points: 5
+        },
+        {
+            id: 1,
+            name: 'Secrets Galore',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            points: 5
+        },
+        {
+            id: 1,
+            name: 'Where is it?',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            points: 5
+        }
+    ];
+
+    useEffect(() => {
+        setChallenges(tempChallenges);
+    }, []);
+
+    return (
+        <TerminalWindow
+            title='Play'
+            prompts={[{ path: '~/rescue-tux', command: './rescue-tux --play' }]}
+        >
+            <section id='team'>
+                <h2>Your Team</h2>
+            </section>
+            <section id='challenges'>
+                {challenges ? (
+                    <>
+                        <h2>Challenges</h2>
+                        <div className={styles.challenges}>
+                            {challenges.map(c => (
+                                <Card key={c.id} challenge={c} />
+                            ))}
+                        </div>
+                    </>
+                ) : (
+                    <>Connection Error :(</>
+                )}
+            </section>
+        </TerminalWindow>
+    );
+}
--- src/pages/rescueTux2022/routes.js
+++ src/pages/rescueTux2022/routes.js
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
+import Challenges from '../rescueTheTux/challenges';
 import HomePage from './homePage/homePage';
 import Login from './login';
+import Play from './play/play';
 import Registration from './registration/registration';
 import TeamRegistration from './teamRegistration/teamRegistration';
 
@@ -9,9 +11,10 @@ export default function RescueTux2022Routes() {
         <Routes>
             <Route index element={<HomePage />} />
             <Route path='/register' element={<Registration />} />
-             <Route path='/create-team' element={<TeamRegistration />} />
+            <Route path='/create-team' element={<TeamRegistration />} />
             <Route path='/login' element={<Login />} />
-            <Route path='/play' element={<div>play</div>} />
+            <Route path='/play' element={<Play />} />
+            <Route path='/challenges' element={<Challenges />} />
             {/* <Route path='/play/my-team' element={<div>my team</div>} />
             <Route path='/play/leaderboard' element={<div>leaderboard</div>} /> */}
         </Routes>
