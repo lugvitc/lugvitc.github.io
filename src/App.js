0584e9861b0adc84bb4aeadb0e0c8025b4f5f8dd
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2022-10-22 12:06:30 +0530

    Added hamburger menu and minor styles edits

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
-import TopBar from './components/topBar/topBar';
-import Dock from './components/dock/dock';
+import TopBar from "./components/topBar/topBar";
+import Dock from "./components/dock/dock";
 
-import SettingsDialog from './components/settingsDialog/settingsDialog';
-import useKeyboardShortcuts from './hooks/useKeyboardShortcuts';
-import UserForm from './pages/recruitment_2022_v2/recruitment';
+import SettingsDialog from "./components/settingsDialog/settingsDialog";
+import useKeyboardShortcuts from "./hooks/useKeyboardShortcuts";
+import UserForm from "./pages/recruitment_2022_v2/recruitment";
 
 export default function App({ children, pages }) {
-    // useKeyboardShortcuts();
+  // useKeyboardShortcuts();
 
-    const [mainTopMargin, setMainTopMargin] = useState('0');
+  const [mainTopMargin, setMainTopMargin] = useState("0");
 
-    const topBarRef = useRef(null);
+  const topBarRef = useRef(null);
 
-    useEffect(() => {
-        const setCorrectMargin = () =>
-            setMainTopMargin(`${topBarRef.current.clientHeight}px`);
+  useEffect(() => {
+    const setCorrectMargin = () =>
+      setMainTopMargin(`${topBarRef.current.clientHeight}px`);
 
-        setCorrectMargin();
-        window.addEventListener('resize', setCorrectMargin);
+    setCorrectMargin();
+    window.addEventListener("resize", setCorrectMargin);
 
-        return () => window.removeEventListener('resize', setCorrectMargin);
-    }, []);
+    return () => window.removeEventListener("resize", setCorrectMargin);
+  }, []);
 
-    return (
-        <>
-            <TopBar refer={topBarRef} topBarLinks={pages} />
-            <SettingsDialog />
-            <main
-                style={{
-                    marginTop: mainTopMargin,
-                    minHeight: `calc(100vh - ${mainTopMargin})`
-                }}
-                id='content'
-            >
-                <Dock />
-                <div id='terminals'>{children}</div>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <TopBar refer={topBarRef} topBarLinks={pages} />
+      <SettingsDialog />
+      <main
+        style={{
+          marginTop: mainTopMargin,
+          minHeight: `calc(100vh - ${mainTopMargin})`,
+        }}
+        id="content"
+      >
+        <Dock />
+        <div id="terminals">{children}</div>
+      </main>
+    </>
+  );
 }
-
