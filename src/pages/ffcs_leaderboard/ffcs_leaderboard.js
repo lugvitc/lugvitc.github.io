509f7319665b05feb031b8c8ac0224e804de2b64
Meetesh Saini <code.meeteshsaini@gmail.com>
2023-03-20 23:23:46 +0530

    Fetching leaderboard data from backend

--- src/pages/ffcs_leaderboard/ffcs_leaderboard.js
+++ src/pages/ffcs_leaderboard/ffcs_leaderboard.js
@@ -5,13 +5,17 @@ import useFetch from "../../hooks/useFetch";
 import { useEffect } from 'react';
 import { useState } from 'react';
 
-var leaderboard_members = {};
+// stores all members data fetched from the backend
+var leaderboard_members = [];
 
 export default function FFCSLeaderboard() {
+    // for applying styles to elements
+    // will be used by jsx
     let styleObj = {
         main: { display: false }
     }
 
+    // member whose info is being displayed now
     let memberObj = {
         name: "Meetesh",
         reg: "21bce1258",
@@ -22,14 +26,20 @@ export default function FFCSLeaderboard() {
         photo_path: "https://api.dicebear.com/5.x/avataaars/svg?scale=50",
         extern_style: styleObj
     }
+
+    // member is passed to jsx part to display it.
     const [member, setMember] = useState(memberObj);
+    // open the popup page on true
     const [openPopup, setOpenPopup] = useState(false);
-    const { apiPost } = useFetch();
+    const { api, apiPost } = useFetch();
 
     const fetchMember = async () => {
-        // apiPost("/endpoint", {
-        //     ""
-        // }).then()
+        await api("/leaderboard",{
+            method: "GET"
+        }).then((response) => response.json())
+        .then((response)=>{
+            leaderboard_members = response
+        })
     }
 
     function updateMember(e) {
@@ -40,11 +50,18 @@ export default function FFCSLeaderboard() {
         setMember(memberObj);
     }
 
+    function close_leaderboard_popup() {
+        setOpenPopup(false);
+    }
+
     useEffect(() => {
         if (openPopup) {
             memberObj.extern_style.main.display = true;
-            setMember(memberObj);
         }
+        else {
+            memberObj.extern_style.main.display = false;
+        }
+        setMember(memberObj);
     }, [openPopup])
 
 
@@ -58,8 +75,9 @@ export default function FFCSLeaderboard() {
             title='Leaderboard'
         >
             <TopCards></TopCards>
+
             <button onClick={() => { setOpenPopup(true); }} >Get</button>
-            <LeaderboardPopUpPage member={member}></LeaderboardPopUpPage>
+            <LeaderboardPopUpPage member={member} close_leaderboard_popup={close_leaderboard_popup}></LeaderboardPopUpPage>
         </TerminalWindow>
     );
 }
