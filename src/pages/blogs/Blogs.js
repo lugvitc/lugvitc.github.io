609222e22bccb0f26210a8d74ddc7bedb72c378e
Animesh Agarwal <49840839+AnimeshAgarwal28@users.noreply.github.com>
2023-05-16 11:12:16 +0530

    Update Blogs.js

--- src/pages/blogs/Blogs.js
+++ src/pages/blogs/Blogs.js
@@ -29,17 +29,29 @@ export default function Blogs() {
                     </div> */}
 
             <TerminalCard
-            title='Blog1'>
+            title='Blog1'
+            date = 'dd/mm/yy'
+            department= 'dep'
+            summary=' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure maiores quaerat necessitatibus deserunt cumque ea architecto suscipit molestiae cupiditate, dolorum enim distinctio numquam nam magni dolores illo, sequi eius sit tenetur mollitia modi, temporibus quod maxime similique? Ratione debitis nam qui eius id dolorem voluptatibus quo et doloribus perspiciatis? Quae dolore veritatis quo eos natus aut laboriosam enim voluptatibus error dignissimos ipsam, aliquam '>
             </TerminalCard>
 
             <TerminalCard
-            title='Blog2'>
+            title='Blog2'
+            date = 'dd/mm/yy'
+            department= 'dep'
+            summary=' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure maiores quaerat necessitatibus deserunt cumque ea architecto suscipit molestiae cupiditate, dolorum enim distinctio numquam nam magni dolores illo, sequi eius sit tenetur mollitia modi, temporibus quod maxime similique? Ratione debitis nam qui eius id dolorem voluptatibus quo et doloribus perspiciatis? Quae dolore veritatis quo eos natus aut laboriosam enim voluptatibus error dignissimos ipsam, aliquam '>
             </TerminalCard>
             <TerminalCard
-            title='Blog3'>
+            title='Blog3'
+            date = 'dd/mm/yy'
+            department= 'dep'
+            summary=' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure maiores quaerat necessitatibus deserunt cumque ea architecto suscipit molestiae cupiditate, dolorum enim distinctio numquam nam magni dolores illo, sequi eius sit tenetur mollitia modi, temporibus quod maxime similique? Ratione debitis nam qui eius id dolorem voluptatibus quo et doloribus perspiciatis? Quae dolore veritatis quo eos natus aut laboriosam enim voluptatibus error dignissimos ipsam, aliquam '>
             </TerminalCard>
             <TerminalCard
-            title='Blog4'>
+            title='Blog4'
+            date = 'dd/mm/yy'
+            department= 'dep'
+            summary=' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure maiores quaerat necessitatibus deserunt cumque ea architecto suscipit molestiae cupiditate, dolorum enim distinctio numquam nam magni dolores illo, sequi eius sit tenetur mollitia modi, temporibus quod maxime similique? Ratione debitis nam qui eius id dolorem voluptatibus quo et doloribus perspiciatis? Quae dolore veritatis quo eos natus aut laboriosam enim voluptatibus error dignissimos ipsam, aliquam '>
             </TerminalCard>
 
 
