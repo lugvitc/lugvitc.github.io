4ec04c53586fb11626c0ff868913c9c0ec051f14
Kushal Agrawal <agrawalskushal@gmail.com>
2023-08-18 21:28:35 +0530

    Fixes

--- src/components/blog_search/blog_search.js
+++ src/components/blog_search/blog_search.js
@@ -1,31 +1,29 @@
-import  "./blog_search.css";
+import "./blog_search.css";
 
-
-
-function chooseBlog( blogs, setter) {
+function chooseBlog(blogs, setter) {
   const sortDropdown = document.querySelector(".blog_sort_button");
   const sortOption = sortDropdown.options[sortDropdown.selectedIndex].value;
   switch (sortOption) {
     case "Category":
-      displayCategoryBlog(blogs, setter,"");
+      displayCategoryBlog(blogs, setter, "");
       break;
     case "Web Development":
-      displayCategoryBlog(blogs, setter,"Web Development");
+      displayCategoryBlog(blogs, setter, "Web Development");
       break;
     case "Cyber Security":
-      displayCategoryBlog(blogs, setter,"Cyber Security");
+      displayCategoryBlog(blogs, setter, "Cyber Security");
       break;
     case "Android Development":
-      displayCategoryBlog(blogs, setter,"Android Development");
+      displayCategoryBlog(blogs, setter, "Android Development");
       break;
     case "UI/UX Design":
-      displayCategoryBlog(blogs, setter,"UI/UX Design");
+      displayCategoryBlog(blogs, setter, "UI/UX Design");
       break;
   }
 }
 
 //
-function searchFunction(blogs,e,setter) {
+function searchFunction(blogs, e, setter) {
   const input = e.currentTarget;
   const filter = input.value.toUpperCase();
   const filteredBlogs = blogs.filter((blogs) => {
@@ -37,55 +35,49 @@ function searchFunction(blogs,e,setter) {
   setter(filteredBlogs);
   console.log(filteredBlogs);
 }
-function displayCategoryBlog(blogs , setter,title) {
-    
-    const sortOption = title;
-    const filteredBlogs = blogs.filter((blogs) => {
-    return (
-      blogs.dept.includes(sortOption) 
-      
-    );
+function displayCategoryBlog(blogs, setter, title) {
+  const sortOption = title;
+  const filteredBlogs = blogs.filter((blogs) => {
+    return blogs.dept.includes(sortOption);
   });
   setter(filteredBlogs);
- 
 }
 
-export default function BlogSearch({ all_blogs ,setter }) {
+export default function BlogSearch({ all_blogs, setter }) {
   return (
     <>
-    <div className="blogSearch">
+      <div className="blogSearch">
         <div className="searchbar">
           <div className="search">
             <input
               type="text"
               id="searchinput"
               onChange={(e) => {
-                searchFunction(all_blogs,e,setter);
+                searchFunction(all_blogs, e, setter);
               }}
               placeholder="Search"
             />
           </div>
         </div>
-          <div className="buttons">
-            <div className="btnGroup">
-              <span>Filter By: </span>
-              <select
-          className="blog_sort_button"
-          onChange={(e) => {
-            chooseBlog(all_blogs,setter);
-          }}
-        >
-                <option>Category</option>
-                <option>Web Development</option>
-                <option>Cyber Security</option>
-                <option>Android Development</option>
-                <option>Machine Learning</option>
-                <option>UI/UX Design</option>
-              </select>
-            </div>
+        <div className="buttons">
+          <div className="btnGroup">
+            <select
+              className="blog_sort_button"
+              onChange={(e) => {
+                chooseBlog(all_blogs, setter);
+              }}
+            >
+              <option>All</option>
+              <option>Web Development</option>
+              <option>Cyber Security</option>
+              <option>Android Development</option>
+              <option>Machine Learning</option>
+              <option>UI/UX Design</option>
+            </select>
           </div>
-    </div>    
-</>
+        </div>
+      </div>
+    </>
   );
 }
 
@@ -122,4 +114,4 @@ export default function BlogSearch({ all_blogs ,setter }) {
 //     </div>
 //   </div>
 // </div>
-// </div>    
+// </div>
