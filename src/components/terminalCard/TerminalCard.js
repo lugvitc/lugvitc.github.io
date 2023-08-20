2fe79fb8c1f3b53b3483ed337adf4f85ec4a9d9e
WizzyGeek <ojasscoding@gmail.com>
2023-08-20 19:46:41 +0530

    Make blog pop up positioned as a child

--- src/components/terminalCard/TerminalCard.js
+++ src/components/terminalCard/TerminalCard.js
@@ -8,86 +8,88 @@ import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 
 export default function TerminalCard({ blogs }) {
-  const [likeCounts, setLikeCounts] = useState([]);
+    const [likeCounts, setLikeCounts] = useState([]);
 
-  useEffect(() => {
-    const initialCounts = blogs.map(() => {
-      return Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
-    });
-    setLikeCounts(initialCounts);
-  }, [blogs]);
+    useEffect(() => {
+        const initialCounts = blogs.map(() => {
+        return Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+        });
+        setLikeCounts(initialCounts);
+    }, [blogs]);
 
-  const handleLikeClick = (index) => {
-    setLikeCounts((prevCounts) => {
-      const newCounts = [...prevCounts];
-      newCounts[index] += 1;
-      return newCounts;
-    });
-  };
+    const handleLikeClick = (index) => {
+        setLikeCounts((prevCounts) => {
+        const newCounts = [...prevCounts];
+        newCounts[index] += 1;
+        return newCounts;
+        });
+    };
 
-  const [showPopup, setShowPopup] = useState(false);
-  const [popupContent, setPopupContent] = useState({});
+    const [showPopup, setShowPopup] = useState(false);
+    const [popupContent, setPopupContent] = useState({});
 
-  const handleReadMoreClick = (content,blogTitle) => {
-    setPopupContent({content:content,title:blogTitle});
-    setShowPopup(true);
-  };
+    const elem_blog_search = document.getElementsByClassName("blogSearch")[0];
 
-  const handleCloseClick = () => {
-    setShowPopup(false);
-  };
+    const handleReadMoreClick = (content,blogTitle) => {
+        setPopupContent({content:content,title:blogTitle});
+        setShowPopup(true);
+        elem_blog_search.hidden = true;
+    };
 
-  return (
-    <>
-      { (blogs.length == 0) && (<h3> No Blogs Available</h3>) }
-      {blogs.map((blog, index) => (
-        
-        <div className={styles.index} key={index}>
-          <div className={`${styles.titlebar} ${util.noSelection}`}>
-            <div className={styles.title}>{blog.title}</div>
-          </div>
+    const handleCloseClick = () => {
+        setShowPopup(false);
+        elem_blog_search.hidden = false;
+    };
 
-          <div className={`${styles.body} ${util.terminalText}`}>
-            <img
-              className={styles.thumbnail}
-              src={blog.thumbnail}
-              alt="niubnei"
-            />
-            <br />
-            <div className="info">
-              <button
-                className="like-button"
-                onClick={() => handleLikeClick(index)}
-              >
-                <span className="icon"></span>
-                <span className="count">{likeCounts[index]}</span>
-              </button>
-              <div className="blog_dept">{blog.tags}</div>
-              <div className="date">{blog.date}</div>
+    return (
+        <>
+        { (blogs.length == 0) && (<h3> No Blogs Available</h3>) }
+        { !showPopup && blogs.map((blog, index) => (
+            <div className={styles.index} key={index}>
+                <div className={`${styles.titlebar} ${util.noSelection}`}>
+                    <div className={styles.title}>{blog.title}</div>
+                </div>
+
+                <div className={`${styles.body} ${util.terminalText}`}>
+                    <img
+                        className={styles.thumbnail}
+                        src={blog.thumbnail}
+                        alt="niubnei"
+                    />
+                    <br />
+                    <div className="info">
+                    <button
+                        className="like-button"
+                        onClick={() => handleLikeClick(index)}
+                    >
+                        <span className="icon"></span>
+                        <span className="count">{likeCounts[index]}</span>
+                    </button>
+                    <div className="blog_dept">{blog.tags}</div>
+                    <div className="date">{blog.date}</div>
+                    </div>
+                    <p></p>
+                    <p>{blog.description}</p>
+                    <button
+                        className="button"
+                        onClick={() => handleReadMoreClick(blog.content,blog.title)}
+                    >
+                    Read More
+                    </button>
+                </div>
             </div>
-            <p></p>
-            <p>{blog.description}</p>
-            <button
-              className="button"
-              onClick={() => handleReadMoreClick(blog.content,blog.title)}
-            >
-              Read More
-            </button>
-          </div>
-        </div>
-      ))}
+        ))}
 
-      {showPopup && (
-        <div className="popup">
-          <div className="blog_title">{popupContent.title}</div>
+        {showPopup && (
+            <div className="popup">
+                <div className="blog_title">{popupContent.title}</div>
+                <ReactMarkdown>{popupContent.content}</ReactMarkdown>
 
-          <ReactMarkdown>{popupContent.content}</ReactMarkdown>
-          
-          <button className="button" onClick={handleCloseClick}>
-            Close
-          </button>
-        </div>
-      )}
-    </>
-  );
+                <button className="button" onClick={handleCloseClick}>
+                    Close
+                </button>
+            </div>
+        )}
+        </>
+    );
 }
