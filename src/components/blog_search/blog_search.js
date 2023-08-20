6c06fce4ca35f05cd66796c3b1e5606e045d0232
WizzyGeek <ojasscoding@gmail.com>
2023-08-20 19:48:39 +0530

    chore: beautify

--- src/components/blog_search/blog_search.js
+++ src/components/blog_search/blog_search.js
@@ -1,92 +1,86 @@
 import  "./blog_search.css";
 
-
-
-function chooseBlog( blogs, setter) {
-  const sortDropdown = document.querySelector(".blog_sort_button");
-  const sortOption = sortDropdown.options[sortDropdown.selectedIndex].value;
-  switch (sortOption) {
-    case "Category":
-      displayCategoryBlog(blogs, setter,"");
-      break;
-    case "Web Development":
-      displayCategoryBlog(blogs, setter,"Web Development");
-      break;
-    case "Cyber Security":
-      displayCategoryBlog(blogs, setter,"Cyber Security");
-      break;
-    case "Android Development":
-      displayCategoryBlog(blogs, setter,"Android Development");
-      break;
-    case "UI/UX Design":
-      displayCategoryBlog(blogs, setter,"UI/UX Design");
-      break;
-  }
+function chooseBlog(blogs, setter) {
+    const sortDropdown = document.querySelector(".blog_sort_button");
+    const sortOption = sortDropdown.options[sortDropdown.selectedIndex].value;
+    switch (sortOption) {
+        case "Category":
+            displayCategoryBlog(blogs, setter,"");
+            break;
+        case "Web Development":
+            displayCategoryBlog(blogs, setter,"Web Development");
+            break;
+        case "Cyber Security":
+            displayCategoryBlog(blogs, setter,"Cyber Security");
+            break;
+        case "Android Development":
+            displayCategoryBlog(blogs, setter,"Android Development");
+            break;
+        case "UI/UX Design":
+            displayCategoryBlog(blogs, setter,"UI/UX Design");
+            break;
+    }
 }
 
-//
 function searchFunction(blogs,e,setter) {
-  const input = e.currentTarget;
-  const filter = input.value.toUpperCase();
-  const filteredBlogs = blogs.filter((blogs) => {
-    return (
-      blogs.title.toUpperCase().includes(filter) ||
-      blogs.description.toUpperCase().includes(filter)
-    );
-  });
-  setter(filteredBlogs);
-  console.log(filteredBlogs);
-}
-function displayCategoryBlog(blogs , setter,title) {
-    
-    const sortOption = title;
+    const input = e.currentTarget;
+    const filter = input.value.toUpperCase();
     const filteredBlogs = blogs.filter((blogs) => {
-    return (
-      blogs.dept.includes(sortOption) 
-      
-    );
-  });
-  setter(filteredBlogs);
- 
+        return (
+            blogs.title.toUpperCase().includes(filter) ||
+            blogs.description.toUpperCase().includes(filter)
+        );
+    });
+    setter(filteredBlogs);
+    console.log(filteredBlogs);
+}
+
+function displayCategoryBlog(blogs , setter,title) {
+        const sortOption = title;
+        const filteredBlogs = blogs.filter((blogs) => {
+        return (
+            blogs.dept.includes(sortOption)
+        );
+    });
+    setter(filteredBlogs);
 }
 
 export default function BlogSearch({ all_blogs ,setter }) {
-  return (
+    return (
     <>
-    <div className="blogSearch">
-        <div className="searchbar">
-          <div className="search">
-            <input
-              type="text"
-              id="searchinput"
-              onChange={(e) => {
-                searchFunction(all_blogs,e,setter);
-              }}
-              placeholder="Search"
-            />
-          </div>
-        </div>
-          <div className="buttons">
-            <div className="btnGroup">
-              <span>Filter By: </span>
-              <select
-          className="blog_sort_button"
-          onChange={(e) => {
-            chooseBlog(all_blogs,setter);
-          }}
-        >
-                <option>Category</option>
-                <option>Web Development</option>
-                <option>Cyber Security</option>
-                <option>Android Development</option>
-                <option>Machine Learning</option>
-                <option>UI/UX Design</option>
-              </select>
+        <div className="blogSearch">
+            <div className="searchbar">
+                <div className="search">
+                    <input
+                        type="text"
+                        id="searchinput"
+                        onChange={(e) => {
+                            searchFunction(all_blogs,e,setter);
+                        }}
+                        placeholder="Search"
+                    />
+                </div>
             </div>
-          </div>
-    </div>    
-</>
-  );
+            <div className="buttons">
+                <div className="btnGroup">
+                    <span>Filter By: </span>
+                    <select
+                        className="blog_sort_button"
+                        onChange={(e) => {
+                            chooseBlog(all_blogs,setter);
+                        }}
+                    >
+                        <option>Category</option>
+                        <option>Web Development</option>
+                        <option>Cyber Security</option>
+                        <option>Android Development</option>
+                        <option>Machine Learning</option>
+                        <option>UI/UX Design</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </>);
 }
 
 // <div className={Styles.blogSearch}>
@@ -122,4 +116,4 @@ export default function BlogSearch({ all_blogs ,setter }) {
 //     </div>
 //   </div>
 // </div>
-// </div>    
+// </div>
