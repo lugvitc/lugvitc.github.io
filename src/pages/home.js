83a804ec4c63e2360c183fa8cdcec02890439784
Dhananjay Chauhan <108969904+Dhananjay-Chauhan@users.noreply.github.com>
2023-10-13 22:38:16 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -56,18 +56,20 @@ export default function Home() {
                     )}
                     text='*************'
                 >
-                    <br />
-                    handlehover          awareness of Free and Open Source Software (FOSS) through Linux
-                    flavours. It believes in developing collaborative open-source
-                    projects.
-                    <br />
-                    It encourages users to understand the systems and softwares they
-                    use on a fundamental level. Which can be achieved only using
-                    non-proprietary tools.
-                    <br />
-                    It aims to get members to contribute to FOSS, make them more
-                    industry ready and to promote GNU/Linux in General.
-                    <br />
+                   <br />
+                <h1>Linux Club VIT, Chennai</h1>
+                The 'Linux Club' of VIT, Chennai was created to spread
+                awareness of Free and Open Source Software (FOSS) through Linux
+                flavours. It believes in developing collaborative open-source
+                projects.
+                <br />
+                It encourages users to understand the systems and softwares they
+                use on a fundamental level. Which can be achieved only using
+                non-proprietary tools.
+                <br />
+                It aims to get members to contribute to FOSS, make them more
+                industry ready and to promote GNU/Linux in General.
+                <br />
                     <span style={{ color: Colors.nord11 }}>L</span>
                     <span style={{ color: Colors.nord12 }}>I</span>
                     <span style={{ color: Colors.nord13 }}>N</span>
